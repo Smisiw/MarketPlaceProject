4675d05fd8bc61b6da0eb5c619712b81aeaee512
AxTerium <kirill.kazancev.2001@mail.ru>
2025-07-15 21:33:56 +0700

    ip location

--- src/test/java/ru/projects/api_gateway/ApiGatewayApplicationTests.java
+++ src/test/java/ru/projects/api_gateway/ApiGatewayApplicationTests.java
@@ -3,7 +3,7 @@ package ru.projects.api_gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = ApiGatewayApplication.class)
+@SpringBootTest(classes = ApiGatewayApplicationTests.class)
 class ApiGatewayApplicationTests {
 
     @Test
